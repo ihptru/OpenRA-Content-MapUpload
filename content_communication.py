81398fed597ab8daae63144564ee5351c59498b7
Igor Popov <ihptru@gmail.com>
2013-11-22 14:37:31 +0400

    add license note

--- content_communication.py
+++ content_communication.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+#
+# Copyright 2012 ihptru (Igor Popov)
+#
+# This file is part of OpenRA-Content-MapUpload, which is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import urllib2
 import hashlib
@@ -20,7 +35,7 @@ class RemoteUpload:
         self.p_id = 0
         try:
             urllib2.urlopen(self.url)
-        except urllib.error.URLError as e:
+        except urllib2.error.URLError as e:
             print(e)
             exit(1)
 
